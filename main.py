69ef42f4192f8a6baf5dc0233012e458869e1c42
LukaszK <kopczynski.l.k@gmail.com>
2022-05-28 14:23:51 +0200

    proba kodu2

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ class Library:
     def get_b(self, book):
         self.publish_y.append(book)
 
-    if temp [0:2] in self.publish_y:
-        self.autor.append(temp[0:2])
-        self.title.append(temp[0])
+        if temp [0:2] in self.publish_y:
+            self.autor.append(temp[0:2])
+            self.title.append(temp[0])
 
-    else:
-        biblioteka.get_b(temp[0:2])
-        self.autor.append(temp[0:2])
-        self.title.append(temp[0])
+        else:
+            biblioteka.get_b(temp[0:2])
+            self.autor.append(temp[0:2])
+            self.title.append(temp[0])
 
     def calculate(self):
         lista = []
         sort = []
 
-    for book in self.publish_y:
-        k = self.autor.count(book)
-        book = [book[0], book[1], k]
-        lista.append(book)
-        sort = sorted(lista, key = lambda x: x[0])
+        for book in self.publish_y:
+            k = self.autor.count(book)
+            book = [book[0], book[1], k]
+            lista.append(book)
+            sort = sorted(lista, key = lambda x: x[0])
  
-    for obj in sort:
-        obj_1 = (obj[0], obj[1], obj[2])
-        print(obj_1)
+        for obj in sort:
+            obj_1 = (obj[0], obj[1], obj[2])
+            print(obj_1)
         
 biblioteka = Library()
 n = int(input())
