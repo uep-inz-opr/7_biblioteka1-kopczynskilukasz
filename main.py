503ee2cc6d0e04b91a0c9ef2ac7d3c76c3ccbb2a
LukaszK <kopczynski.l.k@gmail.com>
2022-05-28 14:32:06 +0200

    proba kodu kolejna, oby ostatnia

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-class Library:
+class Librr:
 
   def __init__(self):
-    self.title = []
     self.autor = []
-    self.pub_y = []
+    self.tytul = []
+    self.rok = []
 
-  def dodaj_e(self, ksiazka):
-    self.pub_y.append(ksiazka)
+  def addbook(self, ksiazka):
+    self.rok.append(ksiazka)
 
-  def dodaj_e(self, egzemplarz):
+  def addmaga(self, egzemplarz):
     temp = list(egzemplarz)
 
-    if temp [0:2] in self.pub_y:
+
+    if temp [0:2] in self.rok:
 
       self.autor.append(temp[0:2])
-      self.title.append(temp[0])
+      self.tytul.append(temp[0])
 
     else:
             
-      biblioteka.dodaj_e(temp[0:2])
+      biblioteka.addbook(temp[0:2])
       self.autor.append(temp[0:2])
-      self.title.append(temp[0])
+      self.tytul.append(temp[0])
 
-  def zliczanie(self):
+
+  def counting(self):
     lista = []
-    posortowane = []
+    sort_ing = []
 
-    for ksiazka in self.pub_y:
+
+    for ksiazka in self.rok:
       k = self.autor.count(ksiazka)
       ksiazka = [ksiazka[0], ksiazka[1], k]
       lista.append(ksiazka)
-      posortowane = sorted(lista, key = lambda x: x[0])
+      sort_ing = sorted(lista, key = lambda x: x[0])
+
     
-    for obj in posortowane:
+    for obj in sort_ing:
       obj_1 = (obj[0], obj[1], obj[2])
       print(obj_1)
-          
-biblioteka = Library()
+      
+biblioteka = Librr()
 n = int(input())
-
 for i in range(0,n):
     wejscie = eval(input())
-    biblioteka.dodaj_e(wejscie)
+    biblioteka.addmaga(wejscie)
 
-biblioteka.zliczanie()
+biblioteka.counting()
