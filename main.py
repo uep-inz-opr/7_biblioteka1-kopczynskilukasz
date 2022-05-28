f26c8646adf1ec6dbcd9ac8ea787503c7e19f5ac
LukaszK <kopczynski.l.k@gmail.com>
2022-05-28 14:27:56 +0200

    proba kodu kolejna

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 class Library:
 
-    def __init__(self):
-        self.title = []
-        self.autor = []
-        self.publish_y = []
+  def __init__(self):
+    self.title = []
+    self.autor = []
+    self.pub_y = []
+
+  def dodaj_e(self, ksiazka):
+    self.pub_y.append(ksiazka)
+
+  def dodaj_e(self, egzemplarz):
+    temp = list(egzemplarz)
+
+    if temp [0:2] in self.pub_y:
+
+      self.autor.append(temp[0:2])
+      self.title.append(temp[0])
+
+    else:
+            
+      biblioteka.dodaj_e(temp[0:2])
+      self.autor.append(temp[0:2])
+      self.title.append(temp[0])
+
+  def zliczanie(self):
+    lista = []
+    posortowane = []
+
+    for ksiazka in self.pub_y:
+      k = self.autor.count(ksiazka)
+      ksiazka = [ksiazka[0], ksiazka[1], k]
+      lista.append(ksiazka)
+      posortowane = sorted(lista, key = lambda x: x[0])
     
-    def get_m(self, magazine):
-        temp = list(magazine)
-
-    def get_b(self, book):
-        self.publish_y.append(book)
-
-        if temp [0:2] in self.publish_y:
-            self.autor.append(temp[0:2])
-            self.title.append(temp[0])
-
-        else:
-            biblioteka.get_b(temp[0:2])
-            self.autor.append(temp[0:2])
-            self.title.append(temp[0])
-
-    def calculate(self):
-        lista = []
-        sort = []
-
-        for book in self.publish_y:
-            k = self.autor.count(book)
-            book = [book[0], book[1], k]
-            lista.append(book)
-            sort = sorted(lista, key = lambda x: x[0])
- 
-        for obj in sort:
-            obj_1 = (obj[0], obj[1], obj[2])
-            print(obj_1)
-        
+    for obj in posortowane:
+      obj_1 = (obj[0], obj[1], obj[2])
+      print(obj_1)
+          
 biblioteka = Library()
 n = int(input())
 
 for i in range(0,n):
     wejscie = eval(input())
-    biblioteka.get_m(wejscie)
-    
-biblioteka.calculate()
+    biblioteka.dodaj_e(wejscie)
+
+biblioteka.zliczanie()
